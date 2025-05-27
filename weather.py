5656b34a733679554842ab6ffd477d1ccd46cf86
bantee-sharma <banteesharma9@gmail.com>
2025-05-27 21:29:40 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,8 +1,20 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain.agents import tool,AgentExecutor,create_react_agent
+import requests
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
+def weather(city:str)->str:
+    "fetch the current weather of city."
+    url = url = "https://api.weatherstack.com/current?access_key={2eab0e8b57aa4c3082a9f22e95baa467}"
+
+    querystring = {"query":"New Delhi"}
+
+
+    respose = requests.get(url,params=querystring)
+    return respose.json()
+
+print(weather("New delhi"))
