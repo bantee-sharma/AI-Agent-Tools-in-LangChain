589c112e668c936ae04e5a2af9270d68bbdcfe89
bantee-sharma <banteesharma9@gmail.com>
2025-06-09 21:35:24 +0530

    Create w.py

---
+++ w.py
@@ -0,0 +1,31 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import tool,AgentExecutor,create_react_agent
+import requests
+from langchain import hub
+from typing import TypedDict
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+class Weather(TypedDict):
+    city:str
+    location:str
+    temperature:str
+
+llm_with = llm.with_structured_output(Weather)
+
+
+def weather(city:str)->str:
+    "fetch the current weather of city."
+    url = url = "https://api.weatherstack.com/current?access_key=2eab0e8b57aa4c3082a9f22e95baa467"
+
+    querystring = {"query":city}
+    respose = requests.get(url,params=querystring)
+    return respose.json()
+
+
+query = input()
+res = llm_with.invoke({"input":query})
+print(res)
