2246e90b934d7136251de3720a39cfa0900c1566
bantee-sharma <banteesharma9@gmail.com>
2025-06-09 21:01:59 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,7 +2,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain.agents import tool, AgentExecutor, create_react_agent
 from langchain import hub
+from langchain_community.tools import DuckDuckGoSearchRun
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+search = DuckDuckGoSearchRun()
+
