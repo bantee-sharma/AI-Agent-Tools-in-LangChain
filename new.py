17f52f17502ec5c4739d03a82663d4ca354c15d4
bantee-sharma <banteesharma9@gmail.com>
2025-06-09 21:10:25 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain.agents import tool, AgentExecutor, create_react_agent
 from langchain import hub
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.output_parsers import StructuredOutputParser
 import requests
 
 load_dotenv()
@@ -10,8 +11,11 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 search = DuckDuckGoSearchRun()
+prompt = hub.pull("hwchase17/react")
 
+#parser = StructuredOutputParser()
 
+@tool
 def weather(city:str)->str:
     "Fetch the current weather"
     url = "https://api.weatherstack.com/current?access_key=2eab0e8b57aa4c3082a9f22e95baa467"
@@ -19,5 +23,9 @@ def weather(city:str)->str:
     response = requests.get(url=url,params=city_name)
     return response.json()
 
-print(weather("noida"))
+tools = [search,weather]
+
+llm_bind = llm.bind_tools(tools)
+
+print(llm_bind({"query" :"weather of noida"}))
 
