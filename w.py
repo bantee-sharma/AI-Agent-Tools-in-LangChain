5dbaaa81cc40e69971d84196e5f6564e2858bad0
bantee-sharma <banteesharma9@gmail.com>
2025-06-09 21:40:21 +0530

    Update w.py

--- w.py
+++ w.py
@@ -9,23 +9,24 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-class Weather(TypedDict):
-    city:str
-    location:str
-    temperature:str
+class Review(TypedDict):
+    summary:str
+    sentiment:str
 
-llm_with = llm.with_structured_output(Weather)
+model = llm.with_structured_output(Review)
 
+review = '''I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
 
-def weather(city:str)->str:
-    "fetch the current weather of city."
-    url = url = "https://api.weatherstack.com/current?access_key=2eab0e8b57aa4c3082a9f22e95baa467"
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
 
-    querystring = {"query":city}
-    respose = requests.get(url,params=querystring)
-    return respose.json()
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
 
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh'''
 
-query = input()
-res = llm_with.invoke(query)
-print(res)
+print(model.invoke(review))
